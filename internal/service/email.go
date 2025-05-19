d8aa26313834a320ce9e9c04aec8535447c67513
Oleksii Sakhnovskyi <oleksii.sakhnovskyi@gamingtec.com>
2025-05-19 15:48:07 +0200

    change email message

--- internal/service/email.go
+++ internal/service/email.go
@@ -16,9 +16,16 @@ func NewEmailService(sender *emailer.EmailSender) *EmailService {
 
 func (s *EmailService) SendConfirmationEmail(to, token string) error {
 	cfg := config.GetConfig()
+	body := fmt.Sprintf(`
+    <p>Click here to confirm your subscription:</p>
+    <p><a href="%s/confirm/%s">Confirm subscription</a></p>
+    <p>Your token is: %s</p>
+    <p>You can use this token to unsubscribe or click here to unsubscribe</p>
+	<p><a href="%s/unsubscribe/%s">Unsubscribe</a></p>
+`, cfg.ServiceDomain, token, token, cfg.ServiceDomain, token)
 	return s.Emailer.Send(
 		[]string{to},
 		"Confirm your subscription",
-		fmt.Sprintf("Click here to confirm: %s/confirm/%s", cfg.ServiceDomain, token),
+		body,
 	)
 }
